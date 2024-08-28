0b922c6c62f3080c7dfd3c90e01e6cf14178c0fb
kevincamussi <kevincamussi@gmail.com>
2024-08-28 17:50:01 -0300

    feat:add first task

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex h-screen items-center justify-center p-4">
+	<div class="space-x-2 rounded-md border border-neutral-300 p-3 shadow-sm">
+		<input type="checkbox" class="cursor-pointer" />
+		<span>Essa é minha tarefa</span>
+	</div>
+</div>
